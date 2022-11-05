35ee2232fb08d2ee9ff568857ce3169c1cafba09
fedebol <fedeboldrini@gamil.com>
2022-11-05 17:35:14 -0300

    Servicio etapa 1/2

--- FNB.Ecommerce/FNB.Ecommerce.Service.WebApi/Program.cs
+++ FNB.Ecommerce/FNB.Ecommerce.Service.WebApi/Program.cs
@@ -1,3 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using AutoMapper;
 using FNB.Ecommerce.Transversal.Common;
 using FNB.Ecommerce.Transvredal.Mapper;
@@ -14,6 +25,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(x => x.AddProfile(new MappingsProfile()));
+builder.Services.AddMvc()
+    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+    .AddJsonOptions(options => { options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver(); });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
