fa185ddc408976166835f30ed55ec70075533e40
fedebol <fedeboldrini@gamil.com>
2022-11-07 16:49:19 -0300

    Falta configuracion de obj.json

--- FNB.Ecommerce/FNB.Ecommerce.Service.WebApi/Program.cs
+++ FNB.Ecommerce/FNB.Ecommerce.Service.WebApi/Program.cs
@@ -19,6 +19,9 @@ using FNB.Ecommerce.Domain.Interface;
 using FNB.Ecommerce.Domain.Core;
 using FNB.Ecommerce.Application.Interface;
 using FNB.Ecommerce.Application.Main;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,14 +29,48 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+//   .AddJsonOptions(options =>
+//{
+//    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+//}); ;
 builder.Services.AddAutoMapper(x => x.AddProfile(new MappingsProfile()));
-builder.Services.AddControllersWithViews()
-                .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = new Newtonsoft.Json.Serialization.DefaultContractResolver());
+//builder.Services.AddMvc()
+//                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+//               .AddJsonOptions(options => { options.SerializerSetting.ContactResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();});
+builder.Services.AddSingleton<IConfiguration>();
+builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>();
+builder.Services.AddScoped<ICustomersApplication, CustomersApplication>();
+builder.Services.AddScoped<ICustomersDomain, CustomersDomain>();
+builder.Services.AddScoped<ICustomersRepository, CustomersRepository>();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+
+        Title = "FNB Technology Service API Market",
+        Description = "A simple example ASP.NET Core Web API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Federico Boldrini",
+            Url = new Uri("https://example.com/"),
+            Email = "",
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Use under LICX",
+            Url = new Uri("https://example.com/license"),
+        }
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
@@ -41,7 +78,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Api Ecommerce V1");
+    });
 }
 
 app.UseAuthorization();
